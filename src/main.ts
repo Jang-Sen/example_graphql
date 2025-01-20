388844ea8c6d4042cdd431081ce8d7e949603a0a
jangsen <dh7895210@naver.com>
2025-01-20 16:46:32 +0900

    Add swagger & validator

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService: ConfigService = app.get(ConfigService);
+  const port = configService.get('BACKEND_PORT') ?? 3000;
 
   const config = new DocumentBuilder()
     .setTitle('Example Graphql')
     .setDescription('')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, documentFactory);
 
-  await app.listen(8000);
+  await app.listen(port);
 }
 
 bootstrap();
